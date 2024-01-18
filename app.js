4fbd3de6e1c70d8d4eb55689b8928e7ca7da9ef8
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-18 09:49:54 +0100

    updated code

--- app.js
+++ app.js
@@ -7,31 +7,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json());
 
 
-app.get('/api/:date?', (req, res) => {
-  const inputDate = req.params.date;
-
-  let dateObject;
-  if (!inputDate) {
-    dateObject = new Date();
-  } else {
-    dateObject = new Date(inputDate);
-  }
-
-  if (isNaN(dateObject.getTime())) {
-    res.json({ error: 'Invalid Date' });
-  } else {
-    res.json({
-      unix: dateObject.getTime(),
-      utc: dateObject.toUTCString(),
-    });
-  }
-});
-
-// app.get("/api/:date", (req, res) => {
-//   req.time = Date.now().toString();
-//   req.timeNow = new Date().toString();
-//   res.json({unix: req.time, utc: req.timeNow})
-// })
+app.get("/api/:date", (req, res) => {
+  req.time = Date.now().toString();
+  req.timeNow = new Date().toString();
+  res.json({unix: req.time, utc: req.timeNow})
+})
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`)
