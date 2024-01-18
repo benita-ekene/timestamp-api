0b8538736a63e327a3b7a1c0d496a96cfc2df626
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-18 10:21:29 +0100

    updated code

--- app.js
+++ app.js
@@ -7,11 +7,32 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json());
 
 
-app.get("/api/:date", (req, res) => {
-  req.params.unix = Date.now().toString();
-  req.params.utc = new Date().toString();
-  res.json({unix: req.params.unix, utc: req.params.utc})
-})
+// app.get("/api/:date", (req, res) => {
+//   req.params.unix = Date.now().toString();
+//   req.params.utc = new Date().toString();
+//   res.json({unix: req.params.unix, utc: req.params.utc})
+// })
+
+app.get('/api/:date?', (req, res) => {
+  try {
+    const dateString = req.params.date || '';
+    const date = new Date(dateString);
+
+    if (isNaN(date.getTime())) {
+      throw new Error('Invalid Date');
+    }
+
+    const response = {
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.error(error); // Log errors for debugging
+    res.status(400).json({ error: 'Invalid Date' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`)
